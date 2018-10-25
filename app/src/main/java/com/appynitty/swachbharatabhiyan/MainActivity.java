d171cb47031b40787e4d4980593350d256a81e76
Big V Telecom <nishitg@appynitty.com>
2018-10-25 10:34:21 +0530

    Project With login UI

---
+++ app/src/main/java/com/appynitty/swachbharatabhiyan/MainActivity.java
@@ -0,0 +1,24 @@
+package com.appynitty.swachbharatabhiyan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.appynitty.swachbharatabhiyanlibrary.activity.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+}
---
+++ swachbharatabhiyanlibrary/src/main/java/com/appynitty/swachbharatabhiyanlibrary/activity/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.appynitty.swachbharatabhiyanlibrary.activity;
+
+import com.appynitty.swachbharatabhiyanlibrary.R;
+import com.mithsoft.lib.activity.BaseActivity;
+
+/**
+ * Created by Richali Pradhan Gupte on 24-10-2018.
+ */
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void generateId() {
+        setContentView(R.layout.activity_login_layout);
+    }
+
+    @Override
+    protected void registerEvents() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
