ebef620bcd514cdcee5fab474dd8e1992fab01c9
Big V Telecom <nishitg@appynitty.com>
2018-10-26 12:14:09 +0530

    webservice changes commit

--- swachbharatabhiyanlibrary/src/main/java/com/appynitty/swachbharatabhiyanlibrary/webservices/LoginWebService.java
+++ swachbharatabhiyanlibrary/src/main/java/com/appynitty/swachbharatabhiyanlibrary/webservices/LoginWebService.java
@@ -4,6 +4,9 @@ import com.appynitty.retrofitconnectionlibrary.pojos.ResultPojo;
 
 import okhttp3.RequestBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -11,8 +14,9 @@ import retrofit2.http.Part;
 public interface LoginWebService {
 
     @POST("api/Account/Login")
+    @FormUrlEncoded
     Call<ResultPojo> saveLoginDetails(@Header("appId") String key,
-                                           @Header("Content-Type") String content_type,
-                                           @Part("userLoginId") RequestBody userName,
-                                           @Part("userPassword") RequestBody userPassword);
+                                      @Header("Content-Type") String content_type,
+                                      @Field("userLoginId") String userName,
+                                      @Field("userPassword") String userPassword);
 }
