5cf5fd295db82fc5a9e9bae0bfff79b53d146a8c
Big V Telecom <nishitg@appynitty.com>
2018-10-27 16:02:25 +0530

    take photo added

---
+++ swachbharatabhiyanlibrary/src/main/java/com/appynitty/swachbharatabhiyanlibrary/activity/TakePhotoActivity.java
@@ -0,0 +1,24 @@
+package com.appynitty.swachbharatabhiyanlibrary.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.BaseAdapter;
+
+import com.mithsoft.lib.activity.BaseActivity;
+
+public class TakePhotoActivity extends BaseActivity {
+
+    @Override
+    protected void generateId() {
+
+    }
+
+    @Override
+    protected void registerEvents() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
