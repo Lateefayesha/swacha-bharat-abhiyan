203f3d26e7b77d14f552d71a700b450cfd829caf
ayan <ayand@appynitty.com>
2018-10-31 17:42:56 +0530

    - created profile page

---
+++ swachbharatabhiyanlibrary/src/main/java/com/appynitty/swachbharatabhiyanlibrary/activity/ProfilePageActivity.java
@@ -0,0 +1,48 @@
+package com.appynitty.swachbharatabhiyanlibrary.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.appynitty.swachbharatabhiyanlibrary.R;
+
+import java.util.Objects;
+
+public class ProfilePageActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        generateId();
+        registerEvents();
+        initData();
+    }
+
+    private void generateId() {
+        setContentView(R.layout.activity_profile_page);
+        toolbar = findViewById(R.id.toolbar);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void registerEvents() {
+    }
+
+    private void initData() {
+    }
+}
