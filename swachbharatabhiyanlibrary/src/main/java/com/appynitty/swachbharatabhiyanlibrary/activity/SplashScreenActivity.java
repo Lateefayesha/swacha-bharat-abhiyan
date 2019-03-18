bdea54c6cabb1855dc16cda9f93b4c6a7dea69d7
appynity <sandeepc@appynity.com>
2019-03-18 15:33:54 +0530

    minor changes

--- swachbharatabhiyanlibrary/src/main/java/com/appynitty/swachbharatabhiyanlibrary/activity/SplashScreenActivity.java
+++ swachbharatabhiyanlibrary/src/main/java/com/appynitty/swachbharatabhiyanlibrary/activity/SplashScreenActivity.java
@@ -43,7 +43,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         mAdapter = new VersionDetailsAdapterClass();
 
         QuickUtils.prefs.save(AUtils.APP_ID, "1");
-        QuickUtils.prefs.save(AUtils.VERSION_CODE, 7);
+        QuickUtils.prefs.save(AUtils.VERSION_CODE, 9);
 
         setDefaultLanguage();
 
